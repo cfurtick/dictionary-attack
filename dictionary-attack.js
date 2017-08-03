aaf94b2f3c4db4c272b7f3eb2ef43478e763ebd5
cfurtick <clairefurtick@gmail.com>
2017-08-03 15:14:21 -0700

    Update dictionary-attack.js

--- dictionary-attack.js
+++ dictionary-attack.js
@@ -17,7 +17,7 @@ window.onload = init;
 
 function checkPassword() {
   
-  var password = document.getElementById("btnSubmit").innerHTML;
+  var password = document.getElementById("pw").innerHTML;
   
   for (var x = 0; x < wordsList.length; x++){
     if password == wordsList[x] {
